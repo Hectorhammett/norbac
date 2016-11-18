316bac755acd3583ab543eb683fd9a2a9be81bd2
Hector Mendoza <Hector Mendoza>
2016-11-18 00:41:01 -0800

    Finished the main structure and Landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,63 @@
+@extends('layout');
+
+@section('mainContent')
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="main-header">¿Quiénes Somos?</h1>
+            <p class="main-text">
+                El antiguo relleno sanitario de Tijuana ha almacenado más de doce millones de
+
+                metros cúbicos de residuos en el sitio durante un lapso mayor a cuarenta años.
+
+                Norbac, la nueva administración, ha tomado el gran reto de controlar los
+
+                impactos ambientales asociados al cese de operaciones en las áreas donde se
+
+                ha llegado a su fase de clausura, y proponiendo dentro de un nuevo proyecto
+
+                el manejo integral de residuos mediante procesos avanzados de segregación
+
+                favoreciendo a las actividades de reuso, reutilización y reciclamiento; el
+
+                proceso que minimiza el volumen de residuos recibidos le precede una fase de
+
+                confinamiento y generación de energía eléctrica a partir del uso del biogás,
+
+                resultado de la metanogénesis de los residuos orgánicos presentes en los
+
+                residuos de manejo especial, representando el proyecto de manejo integral de
+
+                residuos más ambicioso del noroeste de México.
+            </p>
+
+            <h1 class="main-header">Visión</h1>
+            <p class="main-text">
+                Norbac pretende convertirse en la empresa líder para el manejo integral de
+
+                residuos de manejo especial en Baja California, empleando tecnología de
+
+                vanguardia y minimizando los impactos ambientales asociados, brindando la
+
+                opción más amigable con el ambiente.
+            </p>
+
+            <h1 class="main-header">Misión</h1>
+            <p class="main-text">
+                Ofrecer a nuestros clientes la mejor opción de servicio de recolección y manejo
+
+                de residuos de manejo especial, en cumplimiento con la normatividad
+
+                ambiental vigente, bajo los conceptos de calidad y precios competitivos de
+
+                mercado.
+            </p>
+
+            <h1 class="main-header">Valores</h1>
+            <p class="main-text">
+                Norbac cuenta con autorizaciones vigentes para el manejo integral de residuos,
+
+                para más de cincuenta corrientes de residuos de manejo especial.
+            </p>
+        </div>
+    </div>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet"/>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div id="background-image">
+    </div>
+    <div class="debug-size">
+        <p class="visible-xs">XS</p>
+        <p class="visible-sm">S</p>
+        <p class="visible-md">M</p>
+        <p class="visible-lg">L</p>
+    </div>
+    <nav class="navbar navbar-default" id="main-navbar">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="row">
+                <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" id="toggle-button">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#" id="logo-holder"><img src="{{ asset('img/logo_green.png') }}" class="img-responsive"/></a>
+                    </div>
+                </div>
+                <div class="col-sm-8 text-center hidden-xs" id="main-title-holder">
+                    <h1 id="main-title">Biorrelleno Sanitario</h1>
+                </div>
+            </div>
+            <!--  -->
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <!-- Starts main page -->
+    <div class="container main-container">
+        <div class="row tab-holder hidden-xs">
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">¿Quiénes Somos?</p>
+                </div>
+            </a>
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">Manejo de Residuos</p>
+                </div>
+            </a>
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">Contacto</p>
+                </div>
+            </a>
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">Otras Actividades</p>
+                </div>
+            </a>
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">Legislación</p>
+                </div>
+            </a>
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">Estadísticas</p>
+                </div>
+            </a>
+            <a href="#" class="tab">
+                <div>
+                    <p class="tab-text">Galería</p>
+                </div>
+            </a>
+        </div>
+        <div class="row main-row">
+            <div class="col-md-8 col-sm-7">
+                @yield('mainContent')
+            </div>
+            <div class="col-md-4  col-sm-5">
+                <h2 class="text-center interest-title">Páginas de Interés</h2>
+                <div class="interest">
+                    <h4 class="text-primary">Secretaria de Protección al Ambiente del Gobierno del Estado de Baja
+                        California.</h4>
+                    <h4><a href="http://www.spabc.gob.mx/">http://www.spabc.gob.mx/</a></h4>
+                </div>
+
+                <div class="interest">
+                    <h4 class="text-primary">Secretaria de Medio Ambiente y Recursos Naturales</h4>
+                    <h4><a href="https://www.gob.mx/semarnat">https://www.gob.mx/semarnat</a></h4>
+                </div>
+
+                <div class="interest">
+                    <h4 class="text-primary">Procuraduría Federal de Protección al Ambiente</h4>
+                    <h4><a href="http://www.profepa.gob.mx/">http://www.profepa.gob.mx/</a></h4>
+                </div>
+
+                <div class="interest">
+                    <h4 class="text-primary">Metanogenesis a partir de residuos</h4>
+                    <h4><a href="http://www.fao.org/docrep/019/as400s/as400s.pdf">http://www.fao.org/docrep/019/as400s/as400s.pdf</a></h4>
+                </div>
+
+                <div class="interest">
+                    <h4 class="text-primary">-Calentamiento global y control gases de efecto invernadero en México</h4>
+                    <h4><a href="http://cambioclimatico.conanp.gob.mx/">http://cambioclimatico.conanp.gob.mx/</a></h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid bottom-background">
+    
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ asset('js/app.js') }}"></script>
+  </body>
+</html>
