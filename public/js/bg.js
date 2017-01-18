001157c0c65d07f19bf923a22fdecf2bd6c40c85
Hector Mendoza <Hector Mendoza>
2017-01-17 21:44:45 -0800

    -Changed the bg.js

--- public/js/bg.js
+++ public/js/bg.js
@@ -17,9 +17,9 @@ var arrayOfBg = [
 $(document).ready(function(){
     var background = arrayOfBg[Math.floor(Math.random() * 4)];
     var backgroundDiv = $("#background-image")
-    backgroundDiv.css("background-image","url(" + background.img + ")");
+    backgroundDiv.css("background-image","linear-gradient(rgba(17, 15, 53, 0.5), rgba(17, 15, 53, 0.7)),url(" + background.img + ")");
     if(background.position !== undefined){
-        backgroundDiv.css("background-position",background.position);
+        backgroundDiv.css("background-position","0 0," + background.position);
     }
     backgroundDiv.addClass("background-visible");
 })
